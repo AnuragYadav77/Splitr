957d88347d147a718e71d99c758658ae28b52685
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-09-16 16:37:24 +0530

    Fix import statement for ApiError in error handling middleware

--- server/middleware/error.middleware.js
+++ server/middleware/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/ApiError";
+import { ApiError } from "../utils/ApiError.js";
 export const errorHandler = (err,req,res,next)=>{
     //if error is an instance of our custom ApiError
     if(err instanceof ApiError){
